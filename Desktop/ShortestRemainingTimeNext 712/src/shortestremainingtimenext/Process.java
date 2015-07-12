15d0efcf63a6e06abb575e901fa147b89934293d
Gimhani <gimhanieuthpala@gmail.com>
2015-07-12 11:14:15 +0530

    Nuwantha update

---
+++ Desktop/ShortestRemainingTimeNext 712/src/shortestremainingtimenext/Process.java
@@ -0,0 +1,150 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shortestremainingtimenext;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+/**
+ *
+ * @author Gimhani
+ */
+public class Process {
+
+    public int getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(int startTime) {
+        this.startTime = startTime;
+    }
+    private int startTime;
+    private String name;
+    private String processId;
+    private int serviceTime;
+    private int remainingTime;
+    private int turnAroundTime;
+    private int arivalTime;
+    private int endTime;
+    boolean inprocess;
+    private int position;
+    private int waitingTime;
+
+    public int getWaitingTime() {
+        return waitingTime;
+    }
+
+    public void setWaitingTime(int waitingTime) {
+        this.waitingTime = waitingTime;
+    }
+
+    Process() {
+        remainingTime = serviceTime;
+        this.startTime=-1;
+    }
+
+    
+    
+
+    public Process(String name, String processId, int serviceTime, int startTime, int position) {
+        this.name = name;
+        this.processId = processId;
+        this.serviceTime = serviceTime;
+        this.arivalTime = startTime;
+        this.position = position;
+        this.startTime=-1;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTurnAroundTime() {
+        return turnAroundTime;
+    }
+
+    public void setTurnAroundTime(int turnAroundTime) {
+        this.turnAroundTime = turnAroundTime;
+    }
+
+    public Process(String processId, int serviceTime) {
+        this.processId = processId;
+        this.serviceTime = serviceTime;
+        this.remainingTime = serviceTime;
+        this.turnAroundTime = 0;
+    }
+
+    public String getProcessId() {
+        return processId;
+    }
+
+    public void setProcessId(String processId) {
+        this.processId = processId;
+    }
+
+    public int getServiceTime() {
+        return serviceTime;
+    }
+
+    public void setServiceTime(int serviceTime) {
+        this.serviceTime = serviceTime;
+    }
+
+    public int getRemainingTime() {
+        return remainingTime;
+    }
+
+    public void setRemainingTime(int remainingTime) {
+        this.remainingTime = remainingTime;
+    }
+
+    public int getArivalTime() {
+        return arivalTime;
+    }
+
+    public void setArivalTime(int arivalTime) {
+        this.arivalTime = arivalTime;
+    }
+
+    public int getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(int endTime) {
+        this.endTime = endTime;
+    }
+
+    public void calculateRemainingTime() {
+        remainingTime = remainingTime - turnAroundTime;
+
+    }
+
+    public void start() {
+        String CurrentTime = new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().getTime());
+        //this.arivalTime=CurrentTime;    
+    }
+
+    public boolean isInprocess() {
+        return inprocess;
+    }
+
+    public void setInprocess(boolean inprocess) {
+        this.inprocess = inprocess;
+    }
+
+}
---
+++ Desktop/ShortestRemainingTimeNext 712/src/shortestremainingtimenext/ShortestRemainingTimeNext.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package shortestremainingtimenext;
+
+import com.sun.java.swing.plaf.motif.MotifLookAndFeel;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.plaf.metal.MetalLookAndFeel;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+
+/**
+ *
+ * @author Gimhani
+ */
+public class ShortestRemainingTimeNext {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+    }
+    
+}
